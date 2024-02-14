22b82f30e21a499b325d63fb13d2a4a30393cd2b
Dondamart <daniyermo@gmail.com>
2024-02-14 13:11:30 +0100

    home component created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, HomeComponent],
+  template: '<h1>Comencemos<h1>',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
